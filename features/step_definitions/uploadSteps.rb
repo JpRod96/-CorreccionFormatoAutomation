351c5a93353a186c22eb538424ebeeb041223427
JpRod96 <jprodriguez60@gmail.com>
2019-06-02 19:18:45 -0400

    Add pseudo scenario for upload file test, needs human intervention yet :'v

--- features/step_definitions/uploadSteps.rb
+++ features/step_definitions/uploadSteps.rb
@@ -9,6 +9,12 @@ When(/^I click on the UCB image$/) do
   find(:xpath, '//*[@id="demo"]/div/div[1]/center/a/img').click
 end
 
+When("I upload the {string} file, by the Seleccione su archivo PDF button") do |fileName|
+  #find(:xpath,'//*[@id="demo"]/div/div[2]/div[2]/div[1]/center/button').set(File.absolute_path('./features/step_definitions/pdfs/'+fileName))
+  click_button('Seleccione su archivo PDF')
+  sleep(50)
+end
+
 #Then I will click the "Tickets | Houston Dynamo" link
 Then(/^I will click the "([^"]*)" link$/) do |searchLink|
   click_link(searchLink)
@@ -21,4 +27,9 @@ Then(/^I see the message "([^"]*)"$/) do |string|
     end
 end
 
-
+Then("I see the page calibration form") do
+  message = find(:xpath, '//*[@id="demo"]/div/div[2]/div[1]/div/center/h3')
+  if message.text != "Calibración de páginas"
+    	raise "UCB address should be Calibración de páginas"	
+    end
+end
--- features/upload.feature
+++ features/upload.feature
@@ -6,3 +6,8 @@ Feature: As a student
 Scenario: See the welcome page
   Given I am on the homepage
   Then I see the message "Revisa el formato de tu trabajo académico ¡aquí!"
+
+Scenario: Upload a pdf document
+        Given I am on the homepage
+        When I upload the "documentoVacio.pdf" file, by the Seleccione su archivo PDF button 
+        Then I see the page calibration form
