ebbc90a4d24adaec271c24903cc439ddfd3be143
neddyKG <gonzalezneddy@gmail.com>
2019-06-06 06:03:45 -0400

    verifica el primer error de la seccion caratula

--- features/step_definitions/unsuccesfulGeneralReport.rb
+++ features/step_definitions/unsuccesfulGeneralReport.rb
@@ -6,3 +6,12 @@ Then("I see an unsuccesful report table") do
         expect(td['data-icon']).to eq "times"
     end
   end
+
+When("when I click on the {string} section") do |string|
+    page.find('h5', text: 'Carátula').click
+    
+  end
+  
+  Then("the errors in my essay should appear") do
+    page.find(class: 'sidebar__highlight', text: 'Por favor verficar: Fuente: Times New Roman - Tenga Negrilla - Todo esté en mayúsculas.')
+  end
--- features/unsuccesfulGeneralReport.feature
+++ features/unsuccesfulGeneralReport.feature
@@ -9,3 +9,7 @@ Background:
 
 Scenario: Showing an unsuccesful page cover report
     Then I see an unsuccesful report table
+
+Scenario: Showing the cover page errors
+    And when I click on the "Caratula" section
+    Then the errors in my essay should appear
