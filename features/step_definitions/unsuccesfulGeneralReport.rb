1b7f1ee35450998cdb157ada8e07cd55bf681c33
neddyKG <gonzalezneddy@gmail.com>
2019-06-06 16:21:52 -0400

    se separo los errores que detecta de caratula

--- features/step_definitions/unsuccesfulGeneralReport.rb
+++ features/step_definitions/unsuccesfulGeneralReport.rb
@@ -1,18 +1,8 @@
 Then("I see an unsuccesful report table") do
-    infoColumn = 2
-    numberOfRows = 4
+  infoColumn = 2
+  numberOfRows = 4
 
-    page.all('td svg').each do |td|
-        expect(td['data-icon']).to eq "times"
-    end
+  page.all('td svg').each do |td|
+      expect(td['data-icon']).to eq "times"
   end
-
-When("when I click on the {string} section") do |string|
-    page.find('h5', text: string).click
-    
-  end
-
-  Then("the error message {string} in my essay should appear") do |string|
-    page.find(class: 'sidebar__highlight', text: string)
-  end
-  
+end
