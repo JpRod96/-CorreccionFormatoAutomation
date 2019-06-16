e32d9663b6f0c269b21b17826600137084e9ba14
neddyKG <gonzalezneddy@gmail.com>
2019-06-16 09:13:22 -0400

    cleaned the first features and uploadSteps

--- features/step_definitions/uploadSteps.rb
+++ features/step_definitions/uploadSteps.rb
@@ -1,10 +1,9 @@
-#Given I am on the Google homepage
 Given(/^I am on the homepage$/) do
   page.driver.browser.manage.window.maximize
   visit ('https://ucbcba-correccion-formato.herokuapp.com/')
 end
 
-#When I search for "houston dynamo tickets"
+
 When(/^I click on the UCB image$/) do
   find(:xpath, '//*[@id="demo"]/div/div[1]/center/a/img').click
 end
@@ -27,13 +26,6 @@ end
 
 When("I click the Send button") do
   click_button('Enviar »')
-end
-
-Then(/^I see the message "([^"]*)"$/) do |string|
-  message = find(:xpath, '//*[@id="demo"]/div/div[2]/div[1]/div/center/h3')
-  if message.text != string
-    	raise "UCB address should be"+adUCB	
-    end
 end
 
 Then("I see the general report page") do
